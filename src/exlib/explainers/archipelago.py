400f73847550647df5d9c0998ad92e3dfe87b02e
Weiqiu You <weiqiuy@seas.upenn.edu>
2024-04-09 22:05:53 -0400

    Add archipelago multi label

--- src/exlib/explainers/archipelago.py
+++ src/exlib/explainers/archipelago.py
@@ -6,6 +6,7 @@ from .common import *
 from .libs.archipelago.explainer import Archipelago
 from .libs.archipelago.application_utils.image_utils import *
 from .libs.archipelago.application_utils.utils_torch import ModelWrapperTorch
+from .common import patch_segmenter
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -13,11 +14,12 @@ warnings.filterwarnings("ignore")
 class ArchipelagoImageCls(FeatureAttrMethod):
     """ Image classification with integrated gradients
     """
-    def __init__(self, model, top_k=5):
+    def __init__(self, model, top_k=5, segmenter='quickshift'):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model_wrapper = ModelWrapperTorch(model, device)
         super().__init__(model_wrapper)
         self.top_k = top_k
+        self.segmenter = segmenter
 
     def forward(self, x, t, **kwargs):
         bsz = x.shape[0]
@@ -31,42 +33,60 @@ class ArchipelagoImageCls(FeatureAttrMethod):
         mask_weights_all = []
         for i in range(bsz):
             image = x[i].cpu().permute(1,2,0).numpy()
-            if t is None:
+            ti = t[i] if t is not None else None
+            if ti is None:
                 predictions = self.model(np.expand_dims(image,0))
                 class_idx = predictions[0].argsort()[::-1][0]
             else:
-                class_idx = t[i].cpu().item()
+                if len(t[i].shape) == 0 or len(t[i].shape) == 1 and t[i].shape[0] == 1:
+                    class_idx = [t[i].cpu().item()]
+                else:
+                    class_idx = t[i].cpu().numpy().tolist()
 
             baseline = np.zeros_like(image)
-            segments = quickshift(image, kernel_size=3, max_dist=300, ratio=0.2)
+            if self.segmenter == 'quickshift':
+                segments = quickshift(image, kernel_size=3, max_dist=300, ratio=0.2)
+            else:
+                segments = patch_segmenter(image, sz=(8,8))
 
             xf = ImageXformer(image, baseline, segments)
+
             apgo = Archipelago(self.model, data_xformer=xf, output_indices=class_idx, batch_size=20)
             explanation = apgo.explain(top_k=self.top_k)
 
-            expln_scores = np.zeros_like(segments, dtype=float)
-            expln_flat_masks = np.zeros_like(segments, dtype=float)
-            masks = []
-            mask_weights = []
+            expln_scores_i = []
+            expln_flat_masks_i = []
+            masks_i = []
+            mask_weights_i = []
+            for c_i in range(len(class_idx)):
+                expln_scores = np.zeros_like(segments, dtype=float)
+                expln_flat_masks = np.zeros_like(segments, dtype=float)
+                masks = []
+                mask_weights = []
 
+                for e_i, (k, v) in enumerate(sorted(explanation[c_i].items(), key=lambda item: item[1], reverse=True)):
+                    mask = np.zeros_like(segments, dtype=float)
+                    for s_i in k:
+                        expln_scores[segments == s_i] = v
+                        expln_flat_masks[segments == s_i] = e_i
+                        mask[segments == s_i] = 1
+                    masks.append(mask)
+                    mask_weights.append(v)
 
-            for e_i, (k, v) in enumerate(sorted(explanation.items(), key=lambda item: item[1], reverse=True)):
-                mask = np.zeros_like(segments, dtype=float)
-                for s_i in k:
-                    expln_scores[segments == s_i] = v
-                    expln_flat_masks[segments == s_i] = e_i
-                    mask[segments == s_i] = 1
-                masks.append(mask)
-                mask_weights.append(v)
+                expln_scores = torch.tensor(expln_scores).unsqueeze(0).to(x.device)
+                expln_flat_masks = torch.tensor(expln_flat_masks).unsqueeze(0).to(x.device)
+                masks = torch.tensor(masks).to(x.device)
+                mask_weights = torch.tensor(mask_weights).to(x.device)
 
-            expln_scores = torch.tensor(expln_scores).unsqueeze(0).to(x.device)
-            expln_flat_masks = torch.tensor(expln_flat_masks).unsqueeze(0).to(x.device)
-            masks = torch.tensor(masks).to(x.device)
-            mask_weights = torch.tensor(mask_weights).to(x.device)
-            expln_scores_all.append(expln_scores)
-            expln_flat_masks_all.append(expln_flat_masks)
-            masks_all.append(masks)
-            mask_weights_all.append(mask_weights)
+                expln_scores_i.append(expln_scores)
+                expln_flat_masks_i.append(expln_flat_masks)
+                masks_i.append(masks)
+                mask_weights_i.append(mask_weights)
+
+            expln_scores_all.append(torch.cat(expln_scores_i, dim=0))
+            expln_flat_masks_all.append(torch.cat(expln_flat_masks_i, dim=0))
+            masks_all.append(masks_i)
+            mask_weights_all.append(mask_weights_i)
 
         expln_scores = torch.stack(expln_scores_all, dim=0)
         expln_flat_masks = torch.stack(expln_flat_masks_all, dim=0)
