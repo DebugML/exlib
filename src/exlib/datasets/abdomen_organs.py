43f0d87df0337ef327f54db6fc182dc6f6872428
Anton Xue <antonxue@seas.upenn.edu>
2024-04-25 18:07:34 -0400

    save

--- src/exlib/datasets/abdomen_organs.py
+++ src/exlib/datasets/abdomen_organs.py
@@ -1,5 +1,4 @@
 import os
-import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,9 +13,8 @@ import segmentation_models_pytorch as smp
 from ..modules.sop import SOPImageSeg, SOPConfig, get_chained_attr
 
 
-
 # The kinds of splits we can do
-SPLIT_TYPES = ["train", "test", "train_video", "test_video"]
+SPLIT_TYPES = ["train_frame", "test_frame", "train_video", "test_video"]
 
 # Splitting images by video source
 VIDEO_GLOBS = \
@@ -31,26 +29,28 @@ VIDEO_GLOBS = \
 
 #
 class AbdomenOrgans(Dataset):
-    def __init__(self,
-                 data_dir,
-                 images_dirname = "images",
-                 gonogo_labels_dirname = "gonogo_labels",
-                 organ_labels_dirname = "organ_labels",
-                 split = "train",
-                 train_ratio = 0.8,
-                 image_height = 384,  # Default image height / widths
-                 image_width = 640,
-                 image_transforms = None,
-                 label_transforms = None,
-                 split_seed = 1234,
-                 download = False):
+    def __init__(
+        self,
+         data_dir: str,
+         images_dir: str = "images",
+         gonogo_labels_dir: str = "gonogo_labels",
+         organ_labels_dir: str = "organ_labels",
+         split: str = "train_frame",
+         train_ratio: float = 0.8,
+         image_height: float = 384,
+         image_width: float = 640,
+         train_angle_max: float = 60.0,
+         image_transforms = None,
+         label_transforms = None,
+         download: bool = False
+    ):
         if download:
             raise ValueError("download not implemented")
 
         self.data_dir = data_dir
-        self.images_dir = os.path.join(data_dir, images_dirname)
-        self.gonogo_labels_dir = os.path.join(data_dir, gonogo_labels_dirname)
-        self.organ_labels_dir = os.path.join(data_dir, organ_labels_dirname)
+        self.images_dir = os.path.join(data_dir, images_dir)
+        self.gonogo_labels_dir = os.path.join(data_dir, gonogo_labels_dir)
+        self.organ_labels_dir = os.path.join(data_dir, organ_labels_dir)
 
         assert os.path.isdir(self.images_dir)
         assert os.path.isdir(self.gonogo_labels_dir)
@@ -59,90 +59,79 @@ class AbdomenOrgans(Dataset):
         self.split = split
 
         # Split not regarding video
-        torch.manual_seed(split_seed)
-        if split == "train" or split == "test":
-            all_image_filenames = sorted(os.listdir(self.images_dir))
-            num_all, num_train = len(all_image_filenames), int(len(all_image_filenames) * train_ratio)
-            idx_perms = torch.randperm(num_all)
-            todo_idxs = idx_perms[:num_train] if split == "train" else idx_perms[num_train:]
-            self.image_filenames = sorted([all_image_filenames[i] for i in todo_idxs])
+        if split == "train_frame" or split == "test_frame":
+            all_image_files = sorted(os.listdir(self.images_dir))
+            num_all, num_train = len(all_image_files), int(len(all_image_files) * train_ratio)
+            perm = torch.randperm(num_all)
+            idxs = perm[:num_train] if split.startswith("train") else perm[num_train:]
+            self.image_files = sorted([all_image_files[i] for i in idxs])
 
         # Split by the video source
         elif split == "train_video" or split == "test_video":
             num_all, num_train = len(VIDEO_GLOBS), int(len(VIDEO_GLOBS) * train_ratio)
-            idx_perms = torch.randperm(num_all)
-            todo_idxs = idx_perms[:num_train] if "train" in split else idx_perms[num_train:]
+            perm = torch.randperm(num_all)
+            idxs = perm[:num_train] if "train_frame" in split else perm[num_train:]
 
-            image_filenames = []
-            for idx in todo_idxs:
-                image_filenames += glob.glob(os.path.join(self.images_dir, VIDEO_GLOBS[idx]))
-            self.image_filenames = sorted(image_filenames)
+            image_files = []
+            for i in idxs:
+                image_files += glob.glob(os.path.join(self.images_dir, VIDEO_GLOBS[i]))
+            self.image_files = sorted(image_files)
 
         else:
             raise NotImplementedError()
+
+        # Random rotation angles used for the training data
+        self.train_angle_max = train_angle_max
+        self.random_angles = train_angle_max * (torch.rand(len(self.image_files)) * 2 - 1)
 
         self.image_height = image_height
         self.image_width = image_width
 
         # Image transforms
         if image_transforms is None:
-            if "train" in split:
-                self._image_transforms = transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Resize((image_height, image_width), antialias=True),
-                    transforms.RandomRotation(60),
-                ])
-
-                self.image_transforms = lambda image, seed: \
-                        (torch.manual_seed(seed), self._image_transforms(image))[1]
-            else:
-                self.image_transforms = transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Resize((image_height, image_width), antialias=True)
-                ])
+            self.image_transforms = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize((image_height, image_width), antialias=True),
+            ])
         else:
             assert callable(image_transforms)
             self.image_transforms = image_transforms
 
-        # Label transforms
         if label_transforms is None:
-            if "train" in split:
-                self._label_transforms = transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Resize((image_height, image_width), antialias=True),
-                    transforms.RandomRotation(60),
-                ])
-
-                self.label_transforms = lambda label, seed: \
-                        (torch.manual_seed(seed), self._label_transforms(label))[1]
-
-            else:
-                self.label_transforms = transforms.Compose([
-                      transforms.ToTensor(),
-                      transforms.Resize((image_height, image_width), antialias=True)
-                ])
+            self.label_transforms = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize((image_height, image_width), antialias=True)
+            ])
         else:
             assert callable(label_transforms)
             self.label_transforms = label_transforms
 
+
     def __len__(self):
-        return len(self.image_filenames)
+        return len(self.image_files)
+
 
     def __getitem__(self, idx):
-        image_file = os.path.join(self.images_dir, self.image_filenames[idx])
-        organ_label_file = os.path.join(self.organ_labels_dir, self.image_filenames[idx])
-        gonogo_label_file = os.path.join(self.gonogo_labels_dir, self.image_filenames[idx])
+        image_file = os.path.join(self.images_dir, self.image_files[idx])
+        organ_label_file = os.path.join(self.organ_labels_dir, self.image_files[idx])
+        gonogo_label_file = os.path.join(self.gonogo_labels_dir, self.image_files[idx])
 
         # Read image and label
         image = Image.open(image_file).convert("RGB")
         organ_label = Image.open(organ_label_file).convert("L") # L is grayscale
         gonogo_label = Image.open(gonogo_label_file).convert("L")
 
-        if self.split == "train":
-            seed = torch.seed()
-            image = self.image_transforms(image, seed)
-            organ_label = self.label_transforms(organ_label, seed)
-            gonogo_label = self.label_transforms(gonogo_label, seed)
+        if self.split.startswith("train"):
+            image = self.image_transforms(image)
+            organ_label = self.label_transforms(organ_label)
+            gonogo_label = self.label_transforms(gonogo_label)
+
+            # Apply the random rotation
+            angle = self.random_angles[idx].item()
+            image = transforms.functional.rotate(image, angle)
+            organ_label = transforms.functional.rotate(organ_label, angle)
+            gonogo_label = transforms.functional.rotate(gonogo_label, angle)
+
         else:
             image = self.image_transforms(image)
             organ_label = self.label_transforms(organ_label)
