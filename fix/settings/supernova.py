786abd7b232869cebbcd8a7166c0ffb0089c249b
chaehyeon-kim <64060975+chaehyeon-kim@users.noreply.github.com>
2024-06-06 04:16:09 -0400

    update

--- fix/settings/supernova.py
+++ fix/settings/supernova.py
@@ -179,7 +179,7 @@ def get_supernova_scores(baselines = ['chunk']):
                 pred_groups = baseline(valid_length)
                 pred_groups_batch.append(pred_groups)
                 # pred_groups_batch: batch_size * pred_group_num * valid_length
-    
+
             for j in range(len(valid_time_values_batch)): # j: batch size
                 times = valid_time_values_batch[j]
                 fluxes = valid_flux_values_batch[j]
@@ -199,18 +199,67 @@ def get_supernova_scores(baselines = ['chunk']):
                 color_map = plt.get_cmap('rainbow')
                 colors = color_map(np.linspace(0, 1, len(unique_wavelengths)))
                 wavelength_to_color = {w: c for w, c in zip(unique_wavelengths, colors)}
-    
+
                 eps = 1
                 min_len = 3
                 window_size = 50
                 step_size = int(window_size / 2)
                 for k in range(len(pred_groups_batch[j])): # k: number of group
-                    max_lc = calculate_alignment_scores_for_group(times, wavelengths, fluxes, errors, pred_groups_batch[j][k], time_wv, flux_wv, error_wv, eps, min_len, window_size, step_size)
-                    alignment_scores_all.append(max_lc)
-                    # # save plots
-                    # if bi == 0:
-                    #     if k == 8:
-                    #         plot_flux_time_with_error_bars_group(times, wavelengths, fluxes, errors, pred_groups_batch, unique_wavelengths, wavelength_to_color, bi, j, k, max_lc)
+                    group_time = [times[i] for i, value in enumerate(pred_groups_batch[j][k]) if value == 1]
+                    group_slope = []
+                    group_intercept = []
+                    group_threshold = []
+                    group_lc = []
+                    group_p = []
+                    group_f = []
+                    #chunk_time = [[time, time + window_size] for time in crop_time_wv]
+                    chunk_time = []
+                    current_start = group_time[0]
+                    while current_start + window_size < group_time[-1]:
+                        chunk_time.append([current_start, current_start + window_size])
+                        current_start += step_size
+                    chunk_time.append([current_start, current_start + window_size])
+                    for m in range(len(unique_wavelengths)):
+                        chunk_f = 0
+                        chunk_p = 0
+                        crop_time_wv = [time for time in time_wv[unique_wavelengths[m]] if time in group_time]
                         
+                        crop_flux_wv = [flux_wv[unique_wavelengths[m]][i] for i, time in enumerate(time_wv[unique_wavelengths[m]]) if time in group_time]
+                        crop_error_wv = [error_wv[unique_wavelengths[m]][i] for i, time in enumerate(time_wv[unique_wavelengths[m]]) if time in group_time]
+                        slope, intercept = perform_linear_regression(crop_time_wv, crop_flux_wv)
+                        group_slope.append(slope)
+                        group_intercept.append(intercept)
+                        threshold = threshold_f(eps, crop_error_wv, crop_time_wv)
+                        group_threshold.append(threshold)
+                        p_in = 0
+                        pos_f = 0
+                        if len(crop_time_wv) >= min_len:
+                            for time, flux, error in zip(crop_time_wv, crop_flux_wv, crop_error_wv):
+                                predicted_flux = group_slope[m] * time + group_intercept[m]
+                                lower_bound = flux - (eps*error)
+                                upper_bound = flux + (eps*error)
+                                if lower_bound <= predicted_flux <= upper_bound:
+                                    pos_f += 1
+                        if len(crop_time_wv) == 0:
+                            perc = 0
+                        else:
+                            perc = pos_f / len(crop_time_wv)
+                        group_f.append(perc)
+                        for n in range(len(chunk_time)):
+                            chunk_time_wv = [time for time in crop_time_wv if chunk_time[n][0] <= time <= chunk_time[n][1]]
+                            indices = [i for i, time in enumerate(crop_time_wv) if time in chunk_time_wv]
+                            chunk_flux_wv = [crop_flux_wv[i] for i in indices]
+                            chunk_error_wv = [crop_error_wv[i] for i in indices]
+                            if chunk_time_wv:
+                                p_in += 1
+                        group_p.append(p_in / len(chunk_time))
+                    group_lc = []
+                    for p, f in zip(group_p, group_f):
+                        group_lc.append(p * f)
+                    if all(slope <= threshold for slope, threshold in zip(group_slope, group_threshold)):
+                        max_lc = max(group_lc)
+                    else:
+                        max_lc = max(lc for slope, lc, threshold in zip(group_slope, group_lc, group_threshold) if slope > threshold)
+                    alignment_scores_all.append(max_lc)    
     print("score:", sum(alignment_scores_all)/len(alignment_scores_all), len(alignment_scores_all))
     return alignment_scores_all
