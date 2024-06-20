27a175f0ed06a4fe687d58ad429f77d5e612fdba
helenjin <helenjin@seas.upenn.edu>
2024-06-20 13:44:26 -0400

    politeness_helper updated

--- src/exlib/utils/politeness_helper.py
+++ src/exlib/utils/politeness_helper.py
@@ -2,8 +2,16 @@ import numpy as np
 import pandas as pd
 import os
 
-from pathlib import Path
+# from pathlib import Path
+
+from huggingface_hub import hf_hub_download
+import pandas as pd
 
 def load_lexica(language):
     parent_dir = Path(__file__).parent
-    return pd.read_csv(parent_dir / "politeness_lexica/{}_politelex.csv".format(language))
+
+    REPO_ID = "BrachioLab/multilingual_politeness_helper"
+    FILENAME = "{}_politelex.csv".format(language)
+    return pd.read_csv(hf_hub_download(repo_id=REPO_ID, filename=FILENAME, repo_type="dataset"))
+    
+    # return pd.read_csv(parent_dir / "politeness_lexica/{}_politelex.csv".format(language))
