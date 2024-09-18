c7dd76709c3a7cd2f877630f2a2c6a4cde380588
helenjin <helenjin@seas.upenn.edu>
2024-09-18 14:00:27 -0400

    update text settings

--- src/exlib/datasets/emotion.py
+++ src/exlib/datasets/emotion.py
@@ -9,11 +9,12 @@ from datasets import load_dataset
 import torch.nn as nn
 import sentence_transformers
 
+import os
 import sys
 sys.path.append("../src")
 import exlib
 # Baselines
-from exlib.features.text.text_chunk import text_chunk
+from exlib.features.text import *
 from exlib.utils.emotion_helper import project_points_onto_axes, load_emotions
 
 
@@ -59,7 +60,6 @@ class EmotionDataset(torch.utils.data.Dataset):
             'word_list': word_list
         }
 
-
 #classifier for go emotions dataset
 class EmotionClassifier(nn.Module):
     def __init__(self):
@@ -67,13 +67,15 @@ class EmotionClassifier(nn.Module):
         self.model = AutoModel.from_pretrained(MODEL_REPO)
         self.classifier = nn.Linear(768, 28)
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask= None):
         outputs = self.model(input_ids, attention_mask)
         last_hidden_state = outputs.last_hidden_state
         cls_token = last_hidden_state[:, 0, :]
         logits = self.classifier(cls_token)
-        return logits
+        outputs.__setattr__("logits", logits)
+        return outputs
 
+    
 class Metric(nn.Module): 
     def __init__(self, model_name:str="all-mpnet-base-v2"): 
         super(Metric, self).__init__()
@@ -85,33 +87,25 @@ class Metric(nn.Module):
         self.y2 = points[2]
 
     def define_circumplex(self):
-        # Circumplex labels to emotions
-        label_to_emotions = {
-            # Positive valence
-            "PV": ["Happy", "Pleased", "Delighted", "Excited", "Satisfied"],
-            # Negative valence
-            "NV": ["Miserable", "Frustrated", "Sad", "Depressed", "Afraid"],
-            # High arousal
-            "HA": ["Astonished", "Alarmed", "Angry", "Afraid", "Excited"],
-            # Low arousal
-            "LA": ["Tired", "Sleepy", "Calm", "Satisfied", "Depressed"],
-        }
+        emotions = load_emotions()
+        axis_labels = ["NV", "PV", "HA", "LA"]
         axis_points = []
-        for k, v in label_to_emotions.items():
-            emotion_embeddings = self.model.encode(np.array(v))
+        for label in axis_labels:
+            emotion_words = emotions[label]
+            emotion_embeddings = self.model.encode(emotion_words)
             axis_points.append(np.mean(emotion_embeddings, axis=0))
         return axis_points
     
-    def distance_from_circumplex(self, embeddings):
+    def distance_from_circumplex(self, embeddings): # signal
         projection = project_points_onto_axes(embeddings, self.x1, self.x2, self.y1, self.y2)
         x_projections = projection[0]
         y_projections = projection[1]
         distances = []
-        for x, y in zip(x_projections, y_projections):
+        for x, y in zip(x_projections, y_projections):                
             distances.append(np.abs(np.sqrt(x**2 + y**2)-1))
         return 1/np.mean(distances)
 
-    def mean_pairwise_dist(self, embeddings):
+    def mean_pairwise_dist(self, embeddings): # relatedness
         projection = project_points_onto_axes(embeddings, self.x1, self.x2, self.y1, self.y2)
         distances = []
         x_coords = projection[0]
@@ -125,6 +119,9 @@ class Metric(nn.Module):
 
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
+    
+    def tanh(self, x):
+        return 2*self.sigmoid(x) - 1
 
     # input: list of words
     def calculate_group_alignment(self, groups:list, language:str="english"):
@@ -133,23 +130,30 @@ class Metric(nn.Module):
             embeddings = self.model.encode(group)
             circumplex_dist = self.distance_from_circumplex(embeddings)
             if(len(embeddings) == 1): 
-                # alignments.append(circumplex_dist)
-                final_dist = np.exp(-circumplex_dist)
-                alignments.append(final_dist)
+                alignments.append(circumplex_dist)
             else:
                 mean_dist = self.mean_pairwise_dist(embeddings)
-                # print(circumplex_dist, mean_dist)
-                # combined_dist = circumplex_dist*mean_dist
-                combined_dist = np.exp(-circumplex_dist*mean_dist)
+                combined_dist = circumplex_dist*mean_dist
                 alignments.append(combined_dist)
+        alignments = [self.tanh(np.exp(-a)) for a in alignments]
         return alignments
-        
-    def forward(self, zp, x=None, y=None, z=None, reduce=True, **kwargs): 
-        pass
+    
+    def forward(self, group_masks:list, original_data:list, language="english"): # original_data is processed_word_list
+        #create groups
+        groups = []
+        for i in range(len(group_masks)):
+            mask = group_masks[i]
+            group = [original_data[j] for j in range(len(mask)) if mask[j] == 1 and original_data[j] != '']
+            if group != []:
+                groups.append(group)
+        print(groups)
+        return np.mean(self.calculate_group_alignment(groups, language))
 
 
-def get_emotion_scores(baselines = ['word', 'phrase', 'sentence']):
+def get_emotion_scores(baselines = ['word', 'phrase', 'sentence', 'identity', 'random', 'archipelago', 'clustering']):
     dataset = EmotionDataset("test")
+#     dataset = EmotionDataset("train")
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = EmotionClassifier()
     model.to(device)
@@ -157,32 +161,70 @@ def get_emotion_scores(baselines = ['word', 'phrase', 'sentence']):
 
     metric = Metric()
     torch.manual_seed(1234)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+#     dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
+    distinct = 4
+    scaling = 1.5
+    
     all_baselines_scores = {}
     for baseline in baselines:
         print(f"---- {baseline} Level Groups ----")
         
         baseline_scores = []
+        if baseline == 'clustering':
+            utterances_path = 'utterances/emotion_test.pt'
+            if os.path.exists(utterances_path):
+                utterances = torch.load(utterances_path)
+            else:
+                utterances = [' '.join(dataset[i]['word_list']) for i in range(len(dataset))]
+                torch.save(utterances, utterances_path)
+            groups = ClusteringGroups(utterances, distinct=distinct, scaling=scaling)
+        
         for i, batch in enumerate(tqdm(dataloader)):
             word_lists = batch['word_list']
             word_lists = list(map(list, zip(*word_lists)))
             processed_word_lists = []
-            for word_list in word_lists:
-                processed_word_lists.append([word for word in word_list if word != ''])
+#             for word_list in word_lists:
+#                 processed_word_lists.append([word for word in word_list if word != ''])
             
-            for word_list in processed_word_lists:
-                groups = text_chunk(word_list, baseline)
-                # print(groups)
-                alignments = torch.tensor(metric.calculate_group_alignment(groups))
-                score = alignments.mean()
+            if baseline == 'archipelago': # get masks by batch
+                backbone_model = model
+                groups = ArchipelagoGroups(backbone_model, distinct=distinct, scaling=scaling)
+                all_batch_masks = groups(batch)
+               
+            
+            for example in range(len(word_lists)):
+#                 if baseline in ['word', 'phrase', 'sentence']:
+#                     masks = text_chunk(word_lists[example], baseline, return_mask=True)
+                if baseline == 'word':
+                    groups = WordGroups(distinct=distinct, scaling=scaling)
+                    masks = groups(word_lists[example])
+                elif baseline == 'phrase':
+                    groups = PhraseGroups(distinct=distinct, scaling=scaling)
+                    masks = groups(word_lists[example])
+                elif baseline == 'sentence':
+                    groups = SentenceGroups(distinct=distinct, scaling=scaling)
+                    masks = groups(word_lists[example])
+                elif baseline == 'identity':
+                    groups = IdentityGroups()
+                    masks = groups(word_lists[example])
+                elif baseline == 'random':
+                    groups = RandomGroups(distinct=distinct, scaling=scaling)
+                    masks = groups(word_lists[example])
+                elif baseline == 'archipelago': # get score for each example with the already generated masks
+                    masks = all_batch_masks[example]
+                print(word_lists[example])
+                print(len(masks))
+                print(masks)
+                score = metric(masks, word_lists[example])
+#                 print(score)
+
                 baseline_scores.append(score)
-            
-            # if i > 1:
-            #     break
-                
-        # print(baseline_scores)    
-        baseline_scores = torch.stack(baseline_scores)
+#             if i > 50:
+#                 break
+        
+#         print(baseline_scores)    
+        baseline_scores = torch.tensor(baseline_scores)
         all_baselines_scores[baseline] = baseline_scores
     
     # print(all_baselines_scores)
