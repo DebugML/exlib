01f78213fb8996c2537fe634fb4d56708374cc69
Weiqiu You <weiqiuy@seas.upenn.edu>
2024-10-28 00:23:41 -0400

    remove datasets from __init__

--- src/exlib/explainers/__init__.py
+++ src/exlib/explainers/__init__.py
@@ -1,5 +1,5 @@
-from .rise import RiseImageCls
-from .intgrad import IntGradImageCls, IntGradImageSeg
-from .lime import LimeImageCls, LimeImageSeg
-from .shap import ShapImageCls, ShapImageSeg
-from .gradcam import GradCAMImageCls
+# from .rise import RiseImageCls
+# from .intgrad import IntGradImageCls, IntGradImageSeg
+# from .lime import LimeImageCls, LimeImageSeg
+# from .shap import ShapImageCls, ShapImageSeg
+# from .gradcam import GradCAMImageCls
